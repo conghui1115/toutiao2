7e2d7463bf9edbe74c279a574dc0bd5872102ce1
conghui <740364177@qq.com>
2020-05-18 02:30:41 +0800

    验证规则

--- 工作/代码练习/lianxi/vue/04cli/toutiao/src/views/login/index.vue
+++ 工作/代码练习/lianxi/vue/04cli/toutiao/src/views/login/index.vue
@@ -5,9 +5,11 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-    <!-- 放置一个表单 -->
-      <el-form :model='form' :rules="formRules">
+    <!-- 放置一个表单 :model绑定表单  rules表单规则-->
+      <el-form :model='form' :rules="formRules" ref="form">
+        <!-- prop 接收字段属性 -->
         <el-form-item style="margin-top:20px" prop="iphoneNumber">
+          <!-- v-model 双向绑定数据 -->
           <el-input placeholder="请输入手机号" v-model="form.iphoneNumber"></el-input>
         </el-form-item>
         <el-form-item prop="code">
@@ -20,7 +22,7 @@
         </el-form-item>
         <!-- 登陆 -->
         <el-form-item>
-           <el-button type="primary" style="width:100%">登录</el-button>
+           <el-button type="primary" style="width:100%" @click='login'>登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -32,11 +34,51 @@ export default {
   data () {
     return {
       form: {
+        // 字段
         iphoneNumber: '',
         code: '',
-        checked: true
+        checked: false
       },
-      formRules: {}
+      // 自动校验规则
+      formRules: {
+        iphoneNumber: [
+          // required 验证 空 null  undefine 不能验证 false 或者 true,
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { pattern: /^1(3|4|5|6|7|8|9)\d{9}$/, message: '手机号码不正确', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '验证码不为空', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '输入6位数字' }
+        ],
+        checked: [
+          // 自定义校验
+          {
+            validator: function (rules, value, callback) {
+              value ? callback() : callback(new Error('请选择协议'))
+            }
+          }
+        ]
+      }
+
+    }
+  },
+  methods: {
+    // ref 可以获取原生dom 对象
+    // ref 可以获取组对象实例，作用在组件标签，this.$ref获取
+    login () {
+      // el-form 自带validate()方法
+      // 1.回调函数 参数 isOK fields
+      // this.$refs.form.validate(function (isOK) {
+      //   if (isOK) {
+      //     console.log('校验通过')
+      //   } else {
+      //     console.log('校验未通过')
+      //   }
+      // })
+      this.$refs.form.validate().then(() => {
+        // 如果成功通过 校验就会到达 then
+        alert(123)
+      })
     }
   }
 }
