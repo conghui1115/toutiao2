9a749b81ce6a576a4b7965dcd90bf0d836f3581a
conghui <740364177@qq.com>
2020-05-18 10:43:05 +0800

    安装axios并全局使用

--- 工作/代码练习/lianxi/vue/04cli/toutiao/src/main.js
+++ 工作/代码练习/lianxi/vue/04cli/toutiao/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 导入element-ui模块
-import 'element-ui/lib/theme-chalk/index.css'// 全局element-ui css
-Vue.use(ElementUI) // 全局注册 所有位置都可以使用
+import 'element-ui/lib/theme-chalk/index.css' // 全局注册 所有位置都可以使用
+// 引入axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 配置公共请求地址
+Vue.prototype.$axios = axios // 将axios 赋值给Vue对象的原型属性，全部文件可以用，不在一一导入
+// 全局element-ui css
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
