c7f76f60919dc798546db7e5c0f7a4baf391cc1d
conghui <740364177@qq.com>
2020-06-05 00:48:48 +0800

    login初始化

---
+++ Desktop/toutiao2/src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <h1>我是首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.login{
+  width: 100%;
+  height: 100vh;
+  background:url('../../assets/img/pic_bg.png')
+}
+</style>
